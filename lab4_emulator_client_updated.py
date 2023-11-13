caca837e3bf73c16329423d1838a7aba920d3ce7
Lisheng Z <114932372+lisheZou@users.noreply.github.com>
2023-11-12 21:37:13 -0600

    Add files via upload

---
+++ lab4_emulator_client_updated.py
@@ -0,0 +1,98 @@
+# Import SDK packages
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import time
+import json
+import pandas as pd
+import numpy as np
+
+
+#TODO 1: modify the following parameters
+#Starting and end index, modify this
+device_st = 0
+device_end = 5
+
+#Path to the dataset, modify this
+data_path = "data/vehicle{}.csv"
+
+#Path to your certificates, modify this
+certificate_formatter = "certificates/vehicle_{}/vehicle_{}_certificate.pem.crt"
+key_formatter = "certificates/vehicle_{}/vehicle_{}_private.pem.key"
+
+class MQTTClient:
+    def __init__(self, device_id, cert, key):
+        # For certificate based connection
+        self.device_id = str(device_id)
+        self.state = 0
+        self.client = AWSIoTMQTTClient(self.device_id)
+        #TODO 2: modify your broker address
+        self.client.configureEndpoint("a3pg7jhammq7f9-ats.iot.us-east-2.amazonaws.com", 8883)
+        self.client.configureCredentials("certificates/vehicle_0/AmazonRootCA1.pem", key, cert)
+        self.client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        self.client.configureDrainingFrequency(2)  # Draining: 2 Hz
+        self.client.configureConnectDisconnectTimeout(10)  # 10 sec
+        self.client.configureMQTTOperationTimeout(5)  # 5 sec
+        self.client.onMessage = self.customOnMessage
+        
+
+    def customOnMessage(self,message):
+        #TODO3: fill in the function to show your received message
+        print("client {} received payload {} from topic {}".format(self.device_id, message.payload, message.topic))
+
+
+    # Suback callback
+    def customSubackCallback(self,mid, data):
+        #You don't need to write anything here
+        pass
+
+
+    # Puback callback
+    def customPubackCallback(self,mid):
+        #You don't need to write anything here
+        pass
+
+
+    def publish(self, Payload="0.0,0.0,2416.04,0.01,0.72,0.01,42.25,HBEFA3/PC_G_EU4,0.0,1.04,veh0,5330181#0_0,65.15,5.1,!veh0!var#1,14.72,veh_passenger,0.0,18279.94,24533.12"):
+        #TODO4: fill in this function for your publish
+        self.client.subscribeAsync("car", 0, ackCallback=self.customSubackCallback)
+        
+        self.client.publishAsync("LishengTopic", Payload, 0, ackCallback=self.customPubackCallback)
+
+
+
+print("Loading vehicle data...")
+data = []
+for i in range(5):
+    a = pd.read_csv(data_path.format(i))
+    data.append(a)
+
+
+print("Initializing MQTTClients...")
+clients = []
+for device_id in range(device_st, device_end):
+    client = MQTTClient(device_id,certificate_formatter.format(device_id,device_id) ,key_formatter.format(device_id,device_id))
+    client.client.connect()
+    print(device_id)
+    clients.append(client)
+ 
+
+while True:
+    print("send now?")
+    x = input()
+    if x == "s":
+        for i,c in enumerate(clients):
+            c.publish()
+
+    elif x == "d":
+        for c in clients:
+            c.client.disconnect()
+        print("All devices disconnected")
+        exit()
+    else:
+        print("wrong key pressed")
+
+    time.sleep(3)
+
+
+
+
+
